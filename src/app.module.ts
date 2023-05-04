11d4965f95c88dc3a51351d2ef0019968df8ce35
mosco_Tech <sonolamoyosoluwalorun@gmail.com>
2023-05-04 14:12:43 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://mosco_tech:root@cluster0.uz1qq.mongodb.net/nestjsapp?retryWrites=true&w=majority'), CatModule, ProductModule, UserModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://...'), CatModule, ProductModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
