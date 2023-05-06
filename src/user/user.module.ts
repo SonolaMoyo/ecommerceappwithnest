ac500e8ce0b1eb06f4bd96c0be3c06b3366e2514
SonolaMoyo <sonolamoyosoluwalorun@gmail.com>
2023-05-06 21:27:57 +0100

    user module updated

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserController } from './controller/user/user.controller';
 import { User, UserSchema } from './schema/user.schema';
 import { UserService } from './service/user/user.service';
+import { Product, ProductSchema } from 'src/product/schema/product.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{name: User.name, schema: UserSchema}])],
+  imports: [MongooseModule.forFeature([{name: User.name, schema: UserSchema}]), MongooseModule.forFeature([{name: Product.name, schema:ProductSchema}])],
   controllers: [UserController],
   providers: [UserService]
 })
