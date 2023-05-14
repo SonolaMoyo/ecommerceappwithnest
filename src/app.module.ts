3ff739a5d4fdc991e414a4561bf3517127a70789
SonolaMoyo <sonolamoyosoluwalorun@gmail.com>
2023-05-14 17:41:30 +0100

    little changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,19 +7,21 @@ import { CatModule } from './cat/cat.module';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 import { userAuthMiddleware } from './user/middleware/user.middleware';
-//import { UserController } from './user/controller/user/user.controller';
+import * as dotenv from 'dotenv';
 
+dotenv.config()
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://...'), CatModule, ProductModule, UserModule],
+  imports: [MongooseModule.forRoot(`mongodb+srv://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`), CatModule, ProductModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(userAuthMiddleware).forRoutes(
-      { path: 'user/logout', method: RequestMethod.POST },
-      { path: 'user/cart/:productId', method: RequestMethod.POST },
-      {path: 'user/cart/:productId', method: RequestMethod.DELETE},
-      {path: 'user/checkout', method: RequestMethod.GET})
+      // { path: 'user/logout', method: RequestMethod.GET },
+      // { path: 'user/cart/:productId', method: RequestMethod.POST },
+      // {path: 'user/cart/:productId', method: RequestMethod.DELETE},
+      // {path: 'user/checkout', method: RequestMethod.GET}
+      )
   }
 }
